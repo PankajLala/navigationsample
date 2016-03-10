ac3c7d2580e588d0e7bc9b086f89ae23e28f30f9
Pankaj Lala (PAL) <pal@sys.dom>
2016-03-10 16:26:07 +0530

    cleanup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.config(['$locationProvider', function($locationProvider){
   $locationProvider.html5Mode(true);
 }]);
 app.config(function($stateProvider, $urlRouterProvider){
-  // For any unmatched url, redirect to /home
+  // For any unmatched url, redirect to /home aparantly can't be done using stateprovider
   $urlRouterProvider.otherwise("/");
    // Now set up the states
    $stateProvider
@@ -23,7 +23,7 @@ app.config(function($stateProvider, $urlRouterProvider){
            }
        })
        .state('myhome', {
-           url: '/myhome',
+           url: '/myhome/{name}',
            views: {
               'header': {
                templateUrl: '/templates/partials/header.html'
@@ -32,15 +32,20 @@ app.config(function($stateProvider, $urlRouterProvider){
                  templateUrl: '/templates/partials/content.html'
               },
               'tabContent': {
-                 templateUrl: '/templates/myhome.html'
+                 templateUrl: '/templates/myhome.html',
+                 controller: function($scope, $stateParams){
+                   $scope.name = $stateParams.name;
+                 }
                },
                'footer': {
                  templateUrl: '/templates/partials/footer.html'
               }
            }
+
        })
        .state('about', {
            url: '/about',
+           //views in case of multiple named views
            views: {
               'header': {
               templateUrl: '/templates/partials/header.html'
@@ -57,10 +62,38 @@ app.config(function($stateProvider, $urlRouterProvider){
            }
        })
        .state('about.contact', {
-         url: "/contact/{name}/{username}/{email}",
+         url: "/contact/{name}/{username}/{email}", // html for state's tempalte
          templateUrl: "/templates/contact.html",
-         controller: 'ContactController'
-       });
+         controller: 'ContactController' //controller
+       })
+       .state('employee', {
+         url: "/employee/{empId}",
+         views: {
+            'header': {
+              templateUrl: '/templates/partials/header.html'
+            },
+            'content': {
+               templateUrl: '/templates/partials/content.html'
+             },
+             'tabContent': {
+               templateUrl: '/templates/employee.html',
+               resolve: {
+                 employeeInfo: function($stateParams, EmployeeService){
+                   return EmployeeService.find($stateParams.empId);
+                 }
+               },
+               controller: function($scope, $stateParams, employeeInfo){
+                 $scope.empname=employeeInfo.empname;
+                 $scope.level=employeeInfo.level;
+                 $scope.empId= $stateParams.empId;
+               }
+              },
+            'footer': {
+               templateUrl: '/templates/partials/footer.html'
+            }
+         }
+       })
+
 });
 
 app.controller('ContactController', function($scope, $stateParams){
@@ -71,3 +104,19 @@ app.controller('ContactController', function($scope, $stateParams){
   }
   $scope.user = user;
 });
+
+
+app.service('EmployeeService',['$http', function($http){
+  return {
+    find: function(empId){
+      return $http.get('./employeeInfo.json').then(function(response){
+        return response.data;
+      }, function(error){
+
+      });
+    }
+  }
+
+}]);
+
+window.app = app;
